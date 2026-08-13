826f48954f2eb2033c5417f0e2af43260f1e75b0
Rayan Khan <buildwithrayan7@gmail.com>
2026-08-13 18:49:43 +0500

    Upload js/config.js

---
+++ js/config.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL = 'https://lxrrpuytbobhkkzvatew.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4cnJwdXl0Ym9iaGtrenZhdGV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYzODA2NTcsImV4cCI6MjEwMTk1NjY1N30.52m1Z9kTYCQ3Wcv3sLjpiUdbxTWqZyzsRABiuQuYvnQ';
+
+// Initialize Supabase Client
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
