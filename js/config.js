4f008fefe7a04a0f62a0d5148644a92f5489fded
Rayan Khan <buildwithrayan7@gmail.com>
2026-08-14 18:08:29 +0500

    Fix config.js global client variable

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = 'https://lxrrpuytbobhkkzvatew.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4cnJwdXl0Ym9iaGtrenZhdGV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYzODA2NTcsImV4cCI6MjEwMTk1NjY1N30.52m1Z9kTYCQ3Wcv3sLjpiUdbxTWqZyzsRABiuQuYvnQ';
 
-// Initialize Supabase Client
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// Initialize Supabase Client and attach to window so other scripts can access it!
+window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
